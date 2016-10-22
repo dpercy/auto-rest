c7478aee04060359b76313573c8d9abcd6bf8b7c
David Percy <david.percy@mongodb.com>
2016-10-22 19:06:59 -0400

    hacked-up basic auth

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import json
 
-import pymongo
-from bson import json_util, ObjectId
-from flask import Flask, Response, request, url_for
-from flask.views import MethodView
 from werkzeug.routing import BaseConverter
 
+import pymongo
+from bson import ObjectId, json_util
+from flask import Flask, Response, redirect, request, url_for
+from flask.views import MethodView
+
 app = Flask(__name__)
-conn = pymongo.MongoClient('mongodb://localhost:27017/test')
+conn = pymongo.MongoClient('mongodb://localhost:27017/blog')
 db = conn.get_default_database()
+config = json.load(open('example/config.json'))
 
 def json_response(data, **kwargs):
     return Response(
@@ -47,6 +49,32 @@ class CollectionNameConverter(BaseConverter):
 app.url_map.converters['collection'] = CollectionNameConverter
 
 
+@app.before_request
+def my_basic_auth_middleware():
+    """ Super insecure HTTP basic auth!!! """
+    auth = request.authorization
+    nobody = {
+        '_id': None,
+        'username': None,
+        'password': None,
+    }
+    if not auth:
+        request.user = nobody
+    else:
+        user = db['meta.users'].find_one({ 'username': auth.username,
+                                           'password': auth.password })
+        if user is not None:
+            request.user = user
+        else:
+            request.user = nobody
+@app.route('/login')
+def hack_login():
+    """ hack to get browsers to respect basic auth """
+    if request.user['_id'] is None:
+        return Response("please log in", 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
+    return redirect(url_for('.meta_whoami'))
+
+
 @app.route('/')
 def database_summary():
     return json_response({
@@ -60,8 +88,24 @@ def database_summary():
         ]
     })
 
+
+@app.route('/meta.whoami')
+def meta_whoami():
+    return json_response({
+        'user_id': request.user['_id'],
+        'username': request.user['username'],
+    })
+
+
 @app.route('/<collection:collection>', methods=['GET', 'POST'])
 def collection_view(collection):
+    query = {}
+    if collection in config['permissions']:
+        query = { '$and': [
+            query,
+            parse_query(config['permissions'][collection],
+                        user_id=request.user['_id'])
+        ] }
     if request.method == 'GET':
         return json_response({
             'count': db[collection].count(),
@@ -82,9 +126,14 @@ def collection_view(collection):
     else:
         assert False
 
-@app.route('/<collection:collection>/<oid:id>')
+@app.route('/<collection:collection>/<oid:id>', methods=['GET', 'PUT'])
 def document_view(collection, id):
     if request.method == 'GET':
         return json_response(db[collection].find_one({'_id': id}))
+    elif request.method == 'PUT':
+        # http://www.restapitutorial.com/lessons/httpmethods.html
+        # return 201 if created
+        # return full resource representation? redundant?
+        assert False, "TODO"
     else:
         assert False
